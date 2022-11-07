fbf15336237832dd2497cbc6d006ea28efc388a7
nadyaOleynikova <kposhkanadya@yandex.ru>
2022-11-07 23:08:46 +0200

    changing the code

---
+++ src/main/java/Purchase.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
